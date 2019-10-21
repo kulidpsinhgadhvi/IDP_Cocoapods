130ba6ccca9622d84550331094b5c4e97a02c4ef
Kuldipsinh gadhvi <kuldipsinh.gadhvi@volansys.com>
2019-10-21 18:56:23 +0530

    first commit

---
+++ IDP_Cocoapods/IDP_Cocoapods/IDP_Cocoapods.h
@@ -0,0 +1,19 @@
+//
+//  IDP_Cocoapods.h
+//  IDP_Cocoapods
+//
+//  Created by Kuldipsinh Gadhavi on 21/10/19.
+//  Copyright © 2019 Kuldipsinh Gadhavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IDP_Cocoapods.
+FOUNDATION_EXPORT double IDP_CocoapodsVersionNumber;
+
+//! Project version string for IDP_Cocoapods.
+FOUNDATION_EXPORT const unsigned char IDP_CocoapodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDP_Cocoapods/PublicHeader.h>
+
+
---
+++ IDP_Cocoapods/IDP_Cocoapods/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  IDP_Cocoapods
+//
+//  Created by Kuldipsinh Gadhavi on 21/10/19.
+//  Copyright © 2019 Kuldipsinh Gadhavi. All rights reserved.
+//
+
+import Foundation
+public class Service  {
+    init() {}
+    
+    public static func doSomething() -> String {
+        return "do something"
+    }
+}
